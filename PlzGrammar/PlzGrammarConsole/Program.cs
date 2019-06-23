a64ada6971443760f4cd26944a6cee06105e8846
hhjo <kikiki0611@naver.com>
2019-06-23 23:00:04 +0900

    #14 [수정] 기본 IP를 0.0.0.0 으로 수정
    - 서버에서 Local IP가 안 먹는데 이유를 도저히 모르겠음

--- PlzGrammar/PlzGrammarConsole/Program.cs
+++ PlzGrammar/PlzGrammarConsole/Program.cs
@@ -9,25 +9,10 @@ namespace PlzGrammarConsole
 {
     class Program
     {
-        public static string GetLocalIp()
-        {
-            string localIp = "127.0.0.1";
-            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (IPAddress ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    localIp = ip.ToString();
-                    break;
-                }
-            }
-            return localIp;
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string host = EnvironmentVariable.Get<string>("PLZ_SERVER_HOST", defaultValue: GetLocalIp());
+            string host = EnvironmentVariable.Get<string>("PLZ_SERVER_HOST", defaultValue: "0.0.0.0");
             string port = EnvironmentVariable.Get<string>("PLZ_SERVER_PORT", defaultValue: "8089");
             Console.WriteLine($"Server ({host}, {port})"); 
 
